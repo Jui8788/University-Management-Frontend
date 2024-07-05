60d14c990a602ed640920cc9ae7982d0ee9db1c2
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-07-05 23:37:53 +0600

    admin

---
+++ src/routes/routes.tsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+// import About from "../pages/About";
+// import Contact from "../pages/Contact";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import { adminPaths } from "./admin.routes";
+import { routesGenerator } from "../utils/routesGenerator";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   {
+    //     path: "about",
+    //     element: <About />,
+    //   },
+    //   {
+    //     path: "contact",
+    //     element: <Contact />,
+    //   },
+    // ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: routesGenerator(adminPaths),
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: routesGenerator(adminPaths),
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: routesGenerator(adminPaths),
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+export default router;
